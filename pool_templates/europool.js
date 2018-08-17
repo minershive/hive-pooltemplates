b9cf2aac5ebae5091b0e01046bda28484e775882
Jan <34112129+itsmylife44@users.noreply.github.com>
2018-08-17 11:16:08 +0200

    add europool.me

---
+++ pool_templates/europool.js
@@ -0,0 +1,27 @@
+[
+	{
+        "coin": "ETP",
+        "servers": [
+            {
+                "geo": "POOL",
+                "urls": [
+                    "etp.europool.me:3334"
+                ]
+            }
+        ],
+        "miners": {
+            "claymore": {
+                "epools_tpl": "POOL: %URL%, WALLET: %WAL%, WORKER: %WORKER_NAME%, PSW: x",
+                "claymore_user_config": "-allcoins 1\n-allpools 1"
+            },
+            "ethminer": {
+                "cuda": 1,
+                "opencl": 1,
+                "pass": "x",
+                "port": "%URL_PORT%",
+                "server": "stratum1+tcp://%URL_HOST%",
+                "template": "%WAL%.%WORKER_NAME%"
+            }
+        }
+    }
+]
